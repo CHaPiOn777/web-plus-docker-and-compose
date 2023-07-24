556c7c90ce47c374bc02ea56ca6a4afbf9327f59
CHaPiOn777 <89907581+CHaPiOn777@users.noreply.github.com>
2023-07-24 03:33:19 +0300

    исправил баг с портом

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const PORT = process.env.PORT || 3000;
+  const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true })); //преобразовывает типы налету, типизирует вход данные к тому типу которое указано после :
   await app.listen(PORT, () => console.log(`Сервер работает на ${PORT} порту`));
