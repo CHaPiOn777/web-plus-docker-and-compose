911686661aa27425d5a8ee6220766f58aea911f3
CHaPiOn777 <89907581+CHaPiOn777@users.noreply.github.com>
2023-07-25 16:17:05 +0300

    1

--- backend/src/config/configuration.ts
+++ backend/src/config/configuration.ts
@@ -6,8 +6,8 @@ export default () => ({
     host: process.env.POSTGRES_HOST || 'localhost',
     port: parseInt(process.env.POSTGRES_PORT) || 5432,
     nameDB: process.env.POSTGRES_DB || 'nest_project',
-    password: process.env.POSTGRES_PASSWORD || 'student',
-    name: process.env.POSTGRES_username || 'vladislav',
+    password: process.env.POSTGRES_PASSWORD || 'postgres',
+    name: process.env.POSTGRES_username || 'postgres',
   },
   jwt: {
     secret: process.env.SECRET || 'JWT_SECRET',
