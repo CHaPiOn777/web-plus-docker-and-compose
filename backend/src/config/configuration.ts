48d3308aa6408199854cbca363b48ede5995fbde
CHaPiOn777 <89907581+CHaPiOn777@users.noreply.github.com>
2023-07-25 15:45:05 +0300

    1

--- backend/src/config/configuration.ts
+++ backend/src/config/configuration.ts
@@ -3,7 +3,7 @@ export default () => ({
     port: parseInt(process.env.PORT, 10) || 5000,
   },
   database: {
-    host: process.env.POSTGRES_HOST || 'postgres',
+    host: process.env.POSTGRES_HOST || 'localhost',
     port: parseInt(process.env.POSTGRES_PORT) || 5432,
     nameDB: process.env.POSTGRES_DB || 'nest_project',
     password: process.env.POSTGRES_PASSWORD || 'student',
