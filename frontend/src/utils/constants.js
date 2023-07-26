c03dbce36ba3b23e5202cf495d49dc1b0212f9d9
CHaPiOn777 <89907581+CHaPiOn777@users.noreply.github.com>
2023-07-26 23:14:59 +0300

    1

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,4 +1,4 @@
-export const URL = "http://api.kpdvlad.nomoredomains.xyz/";
+export const URL = "https://api.kpdvlad.nomoredomains.nomoreparties.sbs/";
 
 export const MINIMUM_PASSWORD_LENGTH = 3;
 export const MINIMUM_USERNAME_LENGTH = 3;
